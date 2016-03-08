88e3c089dbc6650156ee60bc3e161f63cbf6bc13
dacaiguoguo <dacaiguoguo@gmail.com>
2016-03-08 16:00:32 +0800

    Initial Commit

---
+++ sshcontine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sshcontine
+//
+//  Created by yanguo sun on 16/3/8.
+//  Copyright © 2016年 Lvmama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
