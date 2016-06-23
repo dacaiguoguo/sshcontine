c476a161f1183d0cdb48dcedfa4b5fd775ed22fb
dacaiguoguo <dacaiguoguo@gmail.com>
2016-06-23 10:29:59 +0800

    fix readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # sshcontine
 sshcontine
 mac os x ssh continue check & connection
+将`com.lvmama.sshcontine.plist copy` 到`~/Library/LaunchAgents` 下
+```
+sudo chmod 600 ~/Library/LaunchDaemons/com.lvmama.sshcontine.plist
+
+```
+```
+sudo chown root ~/Library/LaunchDaemons/com.lvmama.sshcontine.plist
+```
+```
+launchctl load ~/Library/LaunchDaemons/com.lvmama.sshcontine.plist
+```
+如果要停止开机运行
+```
+launchctl unload ~/Library/LaunchDaemons/com.lvmama.sshcontine.plist
+```
+
+注意第一次需要手动ssh链接下，第一次链接的时候要输入yes 同意ssh链接。
--- sshcontine/SSHContine.m
+++ sshcontine/SSHContine.m
@@ -13,31 +13,31 @@ extern char **environ;
 - (int)continueSSH:(NSTimer *)timer {
     NSString *selfPath = [timer.userInfo objectForKey:@"selfPath"];
     NSLog(@"%@",selfPath);
-//    NSString *superPath = [selfPath stringByDeletingLastPathComponent];
-//    NSString *sshResult = [superPath stringByAppendingPathComponent:@"sshresult.txt"];
-//    NSString *toShell = [NSString stringWithFormat:@"ps -A | grep ssh > %@",sshResult];
-//    system([toShell UTF8String]);
-//    NSString *resultString = [NSString stringWithContentsOfFile:sshResult encoding:NSUTF8StringEncoding error:nil];
-//    
-//    
-//    if ([resultString rangeOfString:@"ssh -nNTf -D 7000 -i /Users/sunyanguo/awskeypair.pem ec2-user@ec2-52-193-240-141.ap-northeast-1.compute.amazonaws.com"].location != NSNotFound) {
-//        return 0;
-//    }
-//    pid_t pid;
-//    char *argv[] = {
-//        "/usr/bin/ssh",
-//        "-nNTf",
-//        "-D",
-//        "7000",
-//        "-i",
-//        "/Users/sunyanguo/awskeypair.pem",
-//        "ec2-user@ec2-52-193-240-141.ap-northeast-1.compute.amazonaws.com",
-//        NULL
-//    };
-//    
-//    posix_spawn(&pid, argv[0], NULL, NULL, argv, environ);
-//    printf("%d",pid);
-//    waitpid(pid, NULL, 0);
+    NSString *superPath = [selfPath stringByDeletingLastPathComponent];
+    NSString *sshResult = [superPath stringByAppendingPathComponent:@"sshresult.txt"];
+    NSString *toShell = [NSString stringWithFormat:@"ps -A | grep ssh > %@",sshResult];
+    system([toShell UTF8String]);
+    NSString *resultString = [NSString stringWithContentsOfFile:sshResult encoding:NSUTF8StringEncoding error:nil];
+    
+    
+    if ([resultString rangeOfString:@"ssh -nNTf -D 7000 -i /Users/sunyanguo/awskeypair.pem ec2-user@ec2-52-193-240-141.ap-northeast-1.compute.amazonaws.com"].location != NSNotFound) {
+        return 0;
+    }
+    pid_t pid;
+    char *argv[] = {
+        "/usr/bin/ssh",
+        "-nNTf",
+        "-D",
+        "7000",
+        "-i",
+        "/Users/sunyanguo/awskeypair.pem",
+        "ec2-user@ec2-52-193-240-141.ap-northeast-1.compute.amazonaws.com",
+        NULL
+    };
+    
+    posix_spawn(&pid, argv[0], NULL, NULL, argv, environ);
+    printf("%d",pid);
+    waitpid(pid, NULL, 0);
     return 1;
 }
 @end
